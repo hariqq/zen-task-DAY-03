9826b32c747470007689c20f0af2ad776ab1fb8e
hariqq <94283564+hariqq@users.noreply.github.com>
2021-11-20 22:50:49 +0530

    Add files via upload

---
+++ Resume.js
@@ -0,0 +1,19 @@
+//2)	Create your own resume data in JSON format :
+
+var resume = [
+{
+"Objective"      : "To secure a challenging position in a reputable organization to expand my learnings, knowledge, and skills",
+"Name"           : "Jhon",
+"Qualification"  : "BTech IT",
+"passedout year" : "2021",
+"work experience": "Intern at college project center",
+"skills"         : "Ms word, Ms excel, C, C++, Python",
+"Interest"       : "chess,basketball",
+"Reference"      : "ramesh ramesh@gmail.com"
+}];
+for (var i in resume){
+	console.log(i,resume[i]);
+}
+
+                                    
+          
---
+++ for loop.js
@@ -0,0 +1,116 @@
+// 1)	For the given JSON iterate over all for loops (for, for in, for of, forEach) :
+
+//for loop
+var numbers = [{ 
+    "num1":"10",
+    "num2":"20", 
+    "num3":"30",
+    "num4":"54", 
+    "num5":"50"
+    }] 
+for (var i=0; i < numbers.length; i++) {
+   console.log(numbers[i])
+}
+
+//For in  loop:
+var arr=
+[
+    {
+      "id": "1",
+      "name": "Lura Senger",
+      "email": "Xander_Collier@yahoo.com"
+    },
+    {
+      "id": "2",
+      "name": "Wilburn Weber",
+      "email": "Bennett_Kreiger11@yahoo.com"
+    },
+    {
+      "id": "3",
+      "name": "Tyrique Hahn",
+      "email": "Juston.Altenwerth@yahoo.com"
+    },
+    {
+      "id": "4",
+      "name": "Lenny Bailey",
+      "email": "Guiseppe_Hegmann@yahoo.com"
+    },
+    {
+      "id": "5",
+      "name": "Vladimir Keeling",
+      "email": "Louisa_Walsh18@hotmail.com"
+    },
+    {
+      "id": "6",
+      "name": "Kellie Crona",
+      "email": "Chandler_Abernathy@yahoo.com"
+    },
+    {
+      "id": "7",
+      "name": "Carolina White",
+      "email": "Royal50@hotmail.com"
+    },
+    {
+      "id": "8",
+      "name": "Alfredo Conn",
+      "email": "Clarabelle34@hotmail.com"
+    },
+    {
+      "id": "9",
+      "name": "Stan Shanahan",
+      "email": "Lamar.McClure@hotmail.com"
+    },
+    {
+      "id": "10",
+      "name": "Marvin Fay",
+      "email": "Osbaldo58@hotmail.com"
+    },
+    {
+      "id": "11",
+      "name": "Torrance Rau",
+      "email": "Orin45@gmail.com"
+    },
+    {
+      "id": "12",
+      "name": "Harold Gutmann MD",
+      "email": "Alyce.Stracke37@yahoo.com"
+    },
+    {
+      "id": "13",
+      "name": "Taryn Torphy",
+      "email": "Dean_Breitenberg71@hotmail.com"
+    },
+    {
+      "id": "14",
+      "name": "Bryana Lang",
+      "email": "Tatum.Ullrich@yahoo.com"
+    },
+    {
+      "id": "15",
+      "name": "Nyasia Green DDS",
+      "email": "Dino83@gmail.com"
+    }];
+for (var i in arr) {
+   console.log(i,arr[i]);
+   }
+
+//for of
+const array1 = [{
+    "id1":'a',
+    "id2":'b', 
+    "id3":'c'
+    }];
+
+for (const element of array1) {
+  console.log(element);
+}
+
+//for each
+For each loop :
+const numbers = [65, 44, 12, 4];
+numbers.forEach(myFunction)
+
+function myFunction(item, index, arr) {
+  arr[index] = item * 10;
+}
+
